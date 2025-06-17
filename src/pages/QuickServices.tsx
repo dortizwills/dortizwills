16aa9d04b7fbc563302e8e657ccdb9485176f60c
Danny Ortiz-Wills <dortizwills@gmail.com>
2025-06-17 11:56:24 -0700

    Update QuickServices.tsx

--- src/pages/QuickServices.tsx
+++ src/pages/QuickServices.tsx
@@ -397,7 +397,7 @@ const QuickServices = () => {
           loop 
           className="w-full rounded-lg"
         >
-          <source src="/lovable-uploads/Final-Service-Writers.mp4" type="video/mp4" />
+          <source src="public/lovable-uploads/01-express-lane/final-service-writer.mp4" type="video/mp4" />
         </video>
         
           <br></br>
